43880c46b346834ff4110dc542b69db397bfadd0
Max094Reikeb <maxencebekier@gmail.com>
2021-03-17 19:03:50 +0100

    Added advancement "A Whole New World!"

---
+++ src/main/java/net/reikeb/electrona/events/BlockMinedEvent.java
@@ -0,0 +1,35 @@
+package net.reikeb.electrona.events;
+
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.ServerPlayerEntity;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import net.reikeb.electrona.Electrona;
+import net.reikeb.electrona.init.BlockInit;
+
+@Mod.EventBusSubscriber(modid = Electrona.MODID)
+public class BlockMinedEvent {
+
+    @SubscribeEvent
+    public static void onBlockBreak(BlockEvent.BreakEvent event) {
+        ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
+        Block eventBlock = event.getState().getBlock();
+
+        if (eventBlock == BlockInit.TIN_ORE.get()) {
+            Advancement advancement = player.server.getAdvancements().getAdvancement(new ResourceLocation("electrona:a_whole_new_world"));
+            if (advancement == null) System.out.println("Advancement A Whole New World seems to be null");
+            AdvancementProgress advancementProgress = player.getAdvancements().getOrStartProgress(advancement);
+            if (!advancementProgress.isDone()) {
+                for (String criteria : advancementProgress.getRemainingCriteria()) {
+                    player.getAdvancements().award(advancement, criteria);
+                }
+            }
+        }
+    }
+}
