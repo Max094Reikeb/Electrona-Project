59aac82165eefe58663673f60e2c733deb90960c
Max094Reikeb <maxencebekier@gmail.com>
2021-03-18 09:57:37 +0100

    Added Machine Casing

---
+++ src/main/java/net/reikeb/electrona/blocks/MachineCasing.java
@@ -0,0 +1,40 @@
+package net.reikeb.electrona.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.PushReaction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootContext;
+
+import net.minecraftforge.common.ToolType;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MachineCasing extends Block {
+
+    public MachineCasing() {
+        super(Properties.of(Material.METAL)
+                .sound(SoundType.METAL)
+                .strength(5f, 6f)
+                .lightLevel(s -> 0)
+                .harvestLevel(1)
+                .harvestTool(ToolType.PICKAXE)
+                .requiresCorrectToolForDrops());
+    }
+
+    @Override
+    public PushReaction getPistonPushReaction(BlockState state) {
+        return PushReaction.IGNORE;
+    }
+
+    @Override
+    public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+        List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+        if (!dropsOriginal.isEmpty())
+            return dropsOriginal;
+        return Collections.singletonList(new ItemStack(this, 1));
+    }
+}
--- src/main/java/net/reikeb/electrona/init/BlockInit.java
+++ src/main/java/net/reikeb/electrona/init/BlockInit.java
@@ -26,6 +26,7 @@ public class BlockInit {
     // Other blocks
     public static final RegistryObject<TinOre> TIN_ORE = BLOCKS.register("tin_ore", TinOre::new);
     public static final RegistryObject<TinBlock> TIN_BLOCK = BLOCKS.register("tin_block", TinBlock::new);
+    public static final RegistryObject<MachineCasing> MACHINE_CASING = BLOCKS.register("machine_casing", MachineCasing::new);
     public static final RegistryObject<CompressedObsidian> COMPRESSED_OBSIDIAN = BLOCKS.register("compressed_obsidian", CompressedObsidian::new);
 
 }
