c6e36e726d50405cb90ed6f19088e0b3674fa257
Max094Reikeb <maxencebekier@gmail.com>
2021-06-29 03:56:36 +0200

    Added Charge command

---
+++ src/main/java/net/reikeb/electrona/commands/CommandRegistration.java
@@ -0,0 +1,21 @@
+package net.reikeb.electrona.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+
+import net.minecraft.command.CommandSource;
+
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import net.reikeb.electrona.Electrona;
+
+@Mod.EventBusSubscriber(modid = Electrona.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class CommandRegistration {
+
+    @SubscribeEvent
+    public static void onRegisterCommandEvent(RegisterCommandsEvent event) {
+        CommandDispatcher<CommandSource> commandDispatcher = event.getDispatcher();
+        ChargeCommand.register(commandDispatcher);
+    }
+}
