9f9f1c90b04e9982ea7db331a4fe31684b090bc9
FirstMegaGame4 <firstmegagame4@outlook.fr>
2022-03-20 13:47:01 +0100

    Add Tin, Lead, Steel, Steel Tools, and Ore Generation Add Loot Tables Change Mod Description

---
+++ src/main/java/fr/firstmegagame4/electrona/Blocks.java
@@ -0,0 +1,68 @@
+package fr.firstmegagame4.electrona;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.sound.BlockSoundGroup;
+
+public class Blocks {
+
+    public static final Block TIN_ORE = new Block(FabricBlockSettings.of(Material.STONE)
+            .hardness(3.0F)
+            .sounds(BlockSoundGroup.STONE)
+            .requiresTool());
+    public static final BlockItem TIN_ORE_ITEM = new BlockItem(TIN_ORE,
+            new FabricItemSettings().group(Tabs.ELECTRONA_BLOCKS));
+
+    public static final Block LEAD_ORE = new Block(FabricBlockSettings.of(Material.STONE)
+            .hardness(3.0F)
+            .sounds(BlockSoundGroup.STONE)
+            .requiresTool());
+    public static final BlockItem LEAD_ORE_ITEM = new BlockItem(LEAD_ORE,
+            new FabricItemSettings().group(Tabs.ELECTRONA_BLOCKS));
+
+    public static final Block URANIUM_ORE = new Block(FabricBlockSettings.of(Material.STONE)
+            .hardness(3.0F)
+            .sounds(BlockSoundGroup.STONE)
+            .requiresTool());
+    public static final BlockItem URANIUM_ORE_ITEM = new BlockItem(URANIUM_ORE,
+            new FabricItemSettings().group(Tabs.ELECTRONA_BLOCKS));
+
+    public static final Block GRAVITONIUM_ORE = new Block(FabricBlockSettings.of(Material.STONE)
+            .hardness(3.0F)
+            .sounds(BlockSoundGroup.STONE)
+            .requiresTool());
+    public static final BlockItem GRAVITONIUM_ORE_ITEM = new BlockItem(GRAVITONIUM_ORE,
+            new FabricItemSettings().group(Tabs.ELECTRONA_BLOCKS));
+
+    public static final Block TIN_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
+            .hardness(5.0F)
+            .sounds(BlockSoundGroup.METAL)
+            .requiresTool());
+    public static final BlockItem TIN_BLOCK_ITEM = new BlockItem(TIN_BLOCK,
+            new FabricItemSettings().group(Tabs.ELECTRONA_BLOCKS));
+
+    public static final Block LEAD_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
+            .hardness(5.0F)
+            .sounds(BlockSoundGroup.METAL)
+            .requiresTool());
+    public static final BlockItem LEAD_BLOCK_ITEM = new BlockItem(LEAD_BLOCK,
+            new FabricItemSettings().group(Tabs.ELECTRONA_BLOCKS));
+
+    public static final Block STEEL_BLOCK = new Block(FabricBlockSettings.of(Material.METAL)
+            .hardness(5.0F)
+            .sounds(BlockSoundGroup.METAL)
+            .requiresTool());
+    public static final BlockItem STEEL_BLOCK_ITEM = new BlockItem(STEEL_BLOCK,
+            new FabricItemSettings().group(Tabs.ELECTRONA_BLOCKS));
+
+    public static final Block GRAVITONIUM_BLOCK = new Block(FabricBlockSettings.of(Material.STONE)
+            .hardness(5.0F)
+            .sounds(BlockSoundGroup.METAL)
+            .requiresTool());
+    public static final BlockItem GRAVITONIUM_BLOCK_ITEM = new BlockItem(GRAVITONIUM_BLOCK,
+            new FabricItemSettings().group(Tabs.ELECTRONA_BLOCKS));
+
+}
