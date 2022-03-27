9bc0e24905ea67d20631713f0fbeeee6c848b85f
Max094Reikeb <maxencebekier@gmail.com>
2022-03-27 18:38:24 +0200

    Fixed an issue with slots

--- src/main/java/net/reikeb/electrona/tileentities/AbstractTileEntity.java
+++ src/main/java/net/reikeb/electrona/tileentities/AbstractTileEntity.java
@@ -7,14 +7,16 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.Containers;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
 import net.minecraftforge.common.capabilities.Capability;
@@ -26,27 +28,49 @@ import net.reikeb.electrona.utils.ItemHandler;
 
 import java.util.Objects;
 
-public class AbstractTileEntity extends RandomizableContainerBlockEntity {
+public class AbstractTileEntity extends BaseContainerBlockEntity {
 
     public final ItemHandler inventory;
     public int slots;
-    public NonNullList<ItemStack> stacks = NonNullList.<ItemStack>withSize(slots, ItemStack.EMPTY);
+    public NonNullList<ItemStack> items;
 
     protected AbstractTileEntity(BlockEntityType<?> tileEntity, BlockPos pos, BlockState state, int slots) {
         super(tileEntity, pos, state);
 
-        this.inventory = new ItemHandler(slots);
         this.slots = slots;
+        this.inventory = new ItemHandler(slots);
+        this.items = NonNullList.withSize(slots, ItemStack.EMPTY);
     }
 
-    @Override
-    protected NonNullList<ItemStack> getItems() {
-        return this.stacks;
+    public ItemStack getItem(int itemStack) {
+        return this.items.get(itemStack);
     }
 
-    @Override
-    protected void setItems(NonNullList<ItemStack> nonNullList) {
-        this.stacks = stacks;
+    public ItemStack removeItem(int p_58330_, int p_58331_) {
+        return ContainerHelper.removeItem(this.items, p_58330_, p_58331_);
+    }
+
+    public ItemStack removeItemNoUpdate(int p_58387_) {
+        return ContainerHelper.takeItem(this.items, p_58387_);
+    }
+
+    public void setItem(int slot, ItemStack itemStack) {
+        this.items.set(slot, itemStack);
+        if (itemStack.getCount() > this.getMaxStackSize()) {
+            itemStack.setCount(this.getMaxStackSize());
+        }
+    }
+
+    public boolean stillValid(Player p_58340_) {
+        if (this.level.getBlockEntity(this.worldPosition) != this) {
+            return false;
+        } else {
+            return p_58340_.distanceToSqr((double) this.worldPosition.getX() + 0.5D, (double) this.worldPosition.getY() + 0.5D, (double) this.worldPosition.getZ() + 0.5D) <= 64.0D;
+        }
+    }
+
+    public void clearContent() {
+        this.items.clear();
     }
 
     @Override
@@ -104,6 +128,16 @@ public class AbstractTileEntity extends RandomizableContainerBlockEntity {
             if (!inventory.getStackInSlot(i).isEmpty()) {
                 Containers.dropItemStack(world, pos.getX(), pos.getY(), pos.getZ(), inventory.getStackInSlot(i));
             }
+    }
+
+    public boolean isEmpty() {
+        for (ItemStack itemstack : this.items) {
+            if (!itemstack.isEmpty()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
