51e4507f399796ca17a87c6d84d00e73fd5fb477
Max094Reikeb <maxencebekier@gmail.com>
2022-04-02 16:05:59 +0200

    Forgot SprayerWindow

--- src/main/java/net/reikeb/electrona/guis/SprayerWindow.java
+++ src/main/java/net/reikeb/electrona/guis/SprayerWindow.java
@@ -4,18 +4,15 @@ import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
-import net.reikeb.electrona.Electrona;
 import net.reikeb.electrona.containers.SprayerContainer;
+import net.reikeb.electrona.misc.Keys;
 
 public class SprayerWindow extends AbstractWindow<SprayerContainer> {
 
-    private static final ResourceLocation SPRAYER_GUI = Electrona.RL("textures/guis/sprayer_gui.png");
-
     public SprayerWindow(SprayerContainer container, Inventory inv, Component title) {
-        super(container, inv, title, SPRAYER_GUI);
+        super(container, inv, title, Keys.SPRAYER_GUI);
     }
 
     @Override
