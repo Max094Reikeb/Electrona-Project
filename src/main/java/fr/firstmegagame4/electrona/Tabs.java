495bb5267b21f924ef321d158855bb9c0ba60f3f
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-05-11 14:19:15 +0200

    Fix Crash Compatibility with Tech Reborn

--- src/main/java/fr/firstmegagame4/electrona/Tabs.java
+++ src/main/java/fr/firstmegagame4/electrona/Tabs.java
@@ -8,17 +8,17 @@ public class Tabs {
 
     public static final ItemGroup ELECTRONA_MACHINES = FabricItemGroupBuilder.create(
             Utils.ELIdentifier("machines"))
-            .icon(() -> new ItemStack(Blocks.GRAVITONIUM_BLOCK.getItem()))
+            .icon(() -> new ItemStack(Blocks.COMPRESSOR.getItem()))
             .build();
 
     public static final ItemGroup ELECTRONA_BLOCKS = FabricItemGroupBuilder.create(
             Utils.ELIdentifier("blocks"))
-            .icon(() -> new ItemStack(Blocks.STEEL_BLOCK.getIfCreated().getItem()))
+            .icon(() -> new ItemStack(Blocks.STEEL_BLOCK.getIfCreatedOrElse(Blocks.GRAVITONIUM_BLOCK).getItem()))
             .build();
 
     public static final ItemGroup ELECTRONA_ITEMS = FabricItemGroupBuilder.create(
             Utils.ELIdentifier("items"))
-            .icon(() -> new ItemStack(Items.LEAD_INGOT.getIfCreated()))
+            .icon(() -> new ItemStack(Items.LEAD_INGOT.getIfCreatedOrElse(Items.GRAVITONIUM)))
             .build();
 
     public static final ItemGroup ELECTRONA_TOOLS = FabricItemGroupBuilder.create(
